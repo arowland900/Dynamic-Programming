304947776bf26849facc30fc07b177335ab0428e
Alexander Rowland <arowland900@gmail.com>
2021-06-16 16:50:19 -0700

    add fib tabulation

---
+++ tab/fib.js
@@ -0,0 +1,9 @@
+function fib(n){
+    let res = Array(n+1).fill(0)
+    res[1] = 1
+    for(let i = 1; i < n; i++){
+        table[i+1] += table[i]
+        table[i+2] += table[i]
+    }
+    return table[n]
+}
