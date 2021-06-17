d381d3bcc249438097bec63166b4998bbe34af2a
Alexander Rowland <arowland900@gmail.com>
2021-06-16 17:38:33 -0700

    add gridTraveler with memo

--- memo/gridTravel.js
+++ memo/gridTravel.js
@@ -1,5 +1,18 @@
-function gridTraveler(m, n) {
+function gridTraveler(m, n, memo={}) {
+    let k = `${m},${n}`
+
+    if(k in memo) return memo[k]
     if (!m || !n) return 0
     if (m == 1 & n == 1) return 1
-    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+
+    
+    memo[k] = gridTraveler(m - 1, n) + gridTraveler(m, n - 1)
+    return memo[k]
 }
+
+// After Memo:
+
+
+// Before Memo:
+// Time Complexity: O(2^(m+n))
+// Space Complexity: O(n+m)
