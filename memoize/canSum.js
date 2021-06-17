5e084c55f6de46e86c26555eabac5fe47fc3a13e
Alexander Rowland <arowland900@gmail.com>
2021-06-16 18:37:31 -0700

    add canSum with memoization & space + time complexity notes

--- memoize/canSum.js
+++ memoize/canSum.js
@@ -1,11 +1,24 @@
-function canSum(target, numbers) {
-    if (!target) return true
-    if (target < 0) return false
-
-    for (let num of numbers) {
-        let rem = target - num
-        if (canSum(rem, numbers)) return true
+function canSum(target, numbers, memo={}){
+    if(target in memo) return memo[target]
+    if(!target) return true
+    if(target < 0) return false
+  
+    for(let num of numbers){
+      let rem = target - num
+      if(canSum(rem, numbers, memo)){
+        memo[target] = true 
+        return true
+      }
     }
-
+  
+    memo[target] = false
     return false
-}
+  }
+
+// After Memo:
+// Time Complexity: O(n*m)
+// Space Complexity: O(m)
+
+// Before Memo:
+// Time Complexity: O(n^m)
+// Space Complexity: O(m)
