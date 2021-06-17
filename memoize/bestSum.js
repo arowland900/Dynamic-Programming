a2120dbce092192931435bfea6fac00277907f57
Alexander Rowland <arowland900@gmail.com>
2021-06-16 21:54:39 -0700

    add bestSum memoization

--- memoize/bestSum.js
+++ memoize/bestSum.js
@@ -1,4 +1,5 @@
-function howSum(target, numbers) {
+function howSum(target, numbers, memo = {}) {
+    if (target in memo) return memo[target]
     if (!target) return []
     if (target < 0) return null
 
@@ -6,12 +7,17 @@ function howSum(target, numbers) {
 
     for (let num of numbers) {
         let rem = target - num
-        let remRes = howSum(rem, numbers)
+        let remRes = howSum(rem, numbers, memo)
         if (remRes) {
             let newCombo = [...remRes, num]
             if (!best || best.length > newCombo.length) best = newCombo
         }
     }
 
+    memo[target] = best
     return best
 }
+
+
+
+
